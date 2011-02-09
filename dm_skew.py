21bc75879cae27ad4a033dfcf1e213ba5a801ecd
Marco Lui <saffsd@gmail.com>
2011-02-10 10:12:39 +1100

    updated dm_skew with 3 different implementations

--- dm_skew.py
+++ dm_skew.py
@@ -1,9 +1,13 @@
 from hydrat.common.distance_metrics import distance_metric
 import numpy
 
-class dm_skew_new(distance_metric):
-  # TODO: Implement dimensionality reduction as per cosine.
-  __name__ = "skew_new"
+class dm_skew_0(distance_metric):
+  # This version broadcasts the entire skew computation.
+  # This is bad because even after vector reduction, the
+  # resulting vector is fairly sparse, so we end up working
+  # over very many zero values and consuming massive amounts
+  # of memory.
+  __name__ = "skew_0"
 
   def __init__(self, alpha = 0.99):
     distance_metric.__init__(self)
@@ -73,10 +77,10 @@ class dm_skew_new(distance_metric):
     self.logger.debug('Returning Results')
     return retval
 
-class dm_skew_newer(distance_metric):
+class dm_skew_1(distance_metric):
   # This version attempts to avoid wasteful use of memory in the computation of acc_a
   # by returning some of the outer looping to python space
-  __name__ = "skew_newer"
+  __name__ = "skew_1"
 
   def __init__(self, alpha = 0.99):
     distance_metric.__init__(self)
@@ -108,7 +112,12 @@ class dm_skew_newer(distance_metric):
 
     # Calculate term for where v2 is always zero but v1 is not
     #   This is a 1-dimensional vector, one scalar value per v1 instance
-    penalty = numpy.array(v1[:,n1].sum(1))
+    #   In the case where there are no such terms, we set penalty to 0 and let array
+    #   broadcasting take care of it.
+    if len(n1) > 0:
+      penalty = numpy.array(v1[:,n1].sum(1))
+    else:
+      penalty = 0
 
     # Select only shared features from both matrices
     v1 = v1.transpose()[nb].transpose().toarray()
@@ -120,7 +129,7 @@ class dm_skew_newer(distance_metric):
       v1[i] = numpy.ones_like(v1[i])
       sum1[i] = v1[i].sum()
     for i in numpy.flatnonzero(sum2 == 0):
-      v2[i] = numpy.ones_like(v1[i])
+      v2[i] = numpy.ones_like(v2[i])
       sum2[i] = v2[i].sum()
 
     # Calculate term for shared features
@@ -141,6 +150,62 @@ class dm_skew_newer(distance_metric):
     acc_b  = numpy.dot(v1, (v2==0).transpose())
 
     retval = ( acc_a + k * (acc_b + penalty) ) / sum1
+
+    self.logger.debug('Returning Results')
+    return retval
+
+class dm_skew_2(distance_metric):
+  # This version attempts to avoid wasteful use of memory in the computation of acc_a
+  # by returning some of the outer looping to python space
+  # In practice, it is an order of magnitude slower than skew_1
+  __name__ = "skew_2"
+
+  def __init__(self, alpha = 0.99):
+    distance_metric.__init__(self)
+    self.alpha = alpha
+
+  def _params(self):
+    p = distance_metric._params(self)
+    p['alpha'] = self.alpha
+    return p
+
+  def vector_distances(self, v1, v2, thresh=10):
+    orig_feats = v1.shape[1]
+    k = numpy.log(1 / (1 - self.alpha))
+
+    # Calculate instance sums
+    sum1 = numpy.array(v1.sum(1))
+    sum2 = numpy.array(v2.sum(1))
+
+    uniform = numpy.ones((orig_feats,))
+
+    # Calculate term for shared features
+    # TODO: Possibly roll one loop back into numpy for speed
+    # TODO: See if we can avoid toarray to working directly with sparse
+    a = self.alpha
+    sum_ratio = (sum1.astype(float) / sum2.transpose())
+    acc_a = numpy.empty((v1.shape[0], v2.shape[0]))
+    acc_b = numpy.empty((v1.shape[0], v2.shape[0]))
+    for ip, _p in enumerate(v1):
+      # Replace empty distributions with uniform distributions
+      _p = _p.toarray()[0] if _p.nnz != 0 else uniform
+      pg = _p > 0
+      for iq, _q in enumerate(v2):
+        # Replace empty distributions with uniform distributions
+        _q = _q.toarray()[0] if _q.nnz != 0 else uniform
+
+        # Calculate the shared component
+        sf = numpy.logical_and(pg, _q>0)
+        p = _p[sf]
+        q = _q[sf]
+        v = p * numpy.log( p / (sum_ratio[ip,iq] * a * q + (1-a) * p) )
+        acc_a[ip, iq] = numpy.sum(v)
+
+        # This is just the sum of p where q is zero. 
+        uf = numpy.logical_and(pg, numpy.logical_not(_q))
+        acc_b[ip, iq] = numpy.sum(_p * uf)
+        
+    retval = ( acc_a + k * (acc_b) ) / sum1
 
     self.logger.debug('Returning Results')
     return retval
