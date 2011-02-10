9e7712d697df12a99b702fb53bf8e25ba8be8fee
Marco Lui <saffsd@gmail.com>
2011-02-10 12:22:13 +1100

    added another optimization to the batched skew calculation

--- dm_skew.py
+++ dm_skew.py
@@ -183,7 +183,7 @@ class dm_skew_2(distance_metric):
     # TODO: Possibly roll one loop back into numpy for speed
     # TODO: See if we can avoid toarray to working directly with sparse
     a = self.alpha
-    sum_ratio = (sum1.astype(float) / sum2.transpose())
+    sum_ratio = (sum1.astype(float) / sum2.transpose()) # POSSIBLE DIV BY ZERO
     acc_a = numpy.empty((v1.shape[0], v2.shape[0]))
     acc_b = numpy.empty((v1.shape[0], v2.shape[0]))
     for ip, _p in enumerate(v1):
@@ -206,6 +206,82 @@ class dm_skew_2(distance_metric):
         acc_b[ip, iq] = numpy.sum(_p * uf)
         
     retval = ( acc_a + k * (acc_b) ) / sum1
+
+    self.logger.debug('Returning Results')
+    return retval
+
+class dm_skew_3(distance_metric):
+  # Identical to 1, except for a small tweak to avoid producing extra boolean arrays
+  # by returning some of the outer looping to python space
+  __name__ = "skew_3"
+
+  def __init__(self, alpha = 0.99):
+    distance_metric.__init__(self)
+    self.alpha = alpha
+
+  def _params(self):
+    p = distance_metric._params(self)
+    p['alpha'] = self.alpha
+    return p
+
+  def vector_distances(self, v1, v2, thresh=10):
+    self.logger.debug('Creating dense representation')
+    orig_feats = v1.shape[1]
+    k = numpy.log(1 / (1 - self.alpha))
+
+    # Calculate instance sums
+    sum1 = numpy.array(v1.sum(1))
+    sum2 = numpy.array(v2.sum(1))
+
+    # Determine shared features
+    f1 = numpy.asarray( v1.sum(0) ) [0] > 0
+    f2 = numpy.asarray( v2.sum(0) ) [0] > 0
+    nb = numpy.flatnonzero(numpy.logical_and(f1, f2)) # both nonzero
+    n1 = numpy.flatnonzero(numpy.logical_and(f1, numpy.logical_not(f2))) # only f1 nonzero
+      
+    if len(nb) == 0:
+      # No overlapping features.
+      return numpy.empty((v1.shape[0],v2.shape[0]), dtype='float')
+
+    # Calculate term for where v2 is always zero but v1 is not
+    #   This is a 1-dimensional vector, one scalar value per v1 instance
+    #   In the case where there are no such terms, we set penalty to 0 and let array
+    #   broadcasting take care of it.
+    if len(n1) > 0:
+      penalty = numpy.array(v1[:,n1].sum(1))
+    else:
+      penalty = 0
+
+    # Select only shared features from both matrices
+    v1 = v1.transpose()[nb].transpose().toarray()
+    v2 = v2.transpose()[nb].transpose().toarray()
+    self.logger.debug('Reduced matrices from %d to %d features', orig_feats, v1.shape[1] )
+
+    # Replace empty distributions with uniform distributions
+    for i in numpy.flatnonzero(sum1 == 0):
+      v1[i] = numpy.ones_like(v1[i])
+      sum1[i] = v1[i].sum()
+    for i in numpy.flatnonzero(sum2 == 0):
+      v2[i] = numpy.ones_like(v2[i])
+      sum2[i] = v2[i].sum()
+
+    # Calculate term for shared features
+    # TODO: Possibly roll one loop back into numpy for speed
+    a = self.alpha
+    sum_ratio = (sum1.astype(float) / sum2.transpose())
+    acc_a = numpy.empty((v1.shape[0], v2.shape[0]))
+    for ip, _p in enumerate(v1):
+      for iq, _q in enumerate(v2):
+        feats = numpy.logical_and(_p, _q)
+        p = _p[feats]
+        q = _q[feats]
+        v = p * numpy.log( p / (sum_ratio[ip,iq] * a * q + (1-a) * p) )
+        acc_a[ip, iq] = numpy.sum(v)
+
+    # This is just the sum of p where q is zero. 
+    acc_b  = numpy.dot(v1, (v2==0).transpose())
+
+    retval = ( acc_a + k * (acc_b + penalty) ) / sum1
 
     self.logger.debug('Returning Results')
     return retval
