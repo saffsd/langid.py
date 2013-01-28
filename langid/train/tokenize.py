d4d7fc7464d395d6acd903dafd3292ca9b06e156
Marco Lui <saffsd@gmail.com>
2013-01-28 18:07:30 +1100

    added byte-NGram tokenizer

---
+++ langid/train/tokenize.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+"""
+tokenize.py - 
+Tokenizer for langid.py training system. This takes a list of files and tokenizes them
+in parallel.
+
+Marco Lui, January 2013
+
+Copyright 2013 Marco Lui <saffsd@gmail.com>. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of the copyright holder.
+"""
+
+######
+# Default values
+# Can be overriden with command-line options
+######
+MAX_NGRAM_ORDER = 4 # largest order of n-grams to consider
+TOP_DOC_FREQ = 15000 # number of tokens to consider for each order
+NUM_BUCKETS = 64 # number of buckets to use in k-v pair generation
+CHUNKSIZE = 50 # maximum size of chunk (number of files tokenized - less = less memory use)
+
+import os, sys, argparse
+import csv
+import shutil
+import tempfile
+import marshal
+import multiprocessing as mp
+import atexit
+
+from itertools import tee, imap
+from collections import defaultdict
+from contextlib import closing
+
+from common import makedir, chunk
+
+class Tokenizer(object):
+  def __init__(self, min_order=1, max_order=3):
+    self.min_order = min_order
+    self.max_order = max_order
+
+  def __call__(self, seq):
+    min_order = self.min_order
+    max_order = self.max_order
+    t = tee(seq, max_order)
+    for i in xrange(max_order):
+      for j in xrange(i):
+        # advance iterators, ignoring result
+        t[i].next()
+    while True:
+      token = ''.join(tn.next() for tn in t)
+      if len(token) < max_order: break
+      for n in xrange(min_order-1, max_order):
+        yield token[:n+1]
+    for a in xrange(max_order-1):
+      for b in xrange(min_order, max_order-a):
+        yield token[a:a+b]
+
+@atexit.register
+def cleanup():
+  global b_dirs, complete
+  try:
+    if not complete:
+      for d in b_dirs:
+        shutil.rmtree(d)
+  except NameError:
+    # Failed before globals defined, nothing to clean
+    pass
+
+def setup_pass_tokenize(tokenizer, b_dirs):
+  global __tokenizer, __b_dirs
+  __tokenizer = tokenizer
+  __b_dirs = b_dirs
+
+def pass_tokenize(chunk_items):
+  """
+  Chunk files into a doc->term mapping,
+  and simultaneously build a term->df count.
+  The term->df counts are redistributed to
+  buckets via python's in-built hash function.
+  This is basically an inversion step, so that 
+  now we are chunked on the term axis rather
+  than the document axis.
+  """
+  global __maxorder, __b_dirs, __extractor
+  __procname = mp.current_process().name
+  b_freq_lang = [tempfile.mkstemp(prefix=__procname+'-', suffix='.domain', dir=p)[0] for p in __b_dirs]
+  b_freq_domain = [tempfile.mkstemp(prefix=__procname+'-', suffix='.lang', dir=p)[0] for p in __b_dirs]
+  
+  extractor = __tokenizer
+  term_lng_freq = defaultdict(lambda: defaultdict(int))
+  term_dom_freq = defaultdict(lambda: defaultdict(int))
+
+  for doc_index, (domain_id, lang_id, path) in enumerate(chunk_items):
+    with open(path) as f:
+      tokenset = set(extractor(f.read()))
+      for token in tokenset:
+        term_lng_freq[token][lang_id] += 1
+        term_dom_freq[token][domain_id] += 1
+
+  for term in term_lng_freq:
+    bucket_index = hash(term) % len(b_freq_lang)
+    for lang, count in term_lng_freq[term].iteritems():
+      os.write(b_freq_lang[bucket_index], marshal.dumps((term, lang, count)))
+    for domain, count in term_dom_freq[term].iteritems():
+      os.write(b_freq_domain[bucket_index], marshal.dumps((term, domain, count)))
+
+  # Close all the open files
+  for f in b_freq_lang + b_freq_domain:
+    os.close(f)
+
+  return len(term_lng_freq)
+
+def build_index(items, outdir, buckets=NUM_BUCKETS, jobs=None, chunksize=CHUNKSIZE, max_order=MAX_NGRAM_ORDER ):
+  """
+  @param items a list of (language, path) pairs
+  """
+  global b_dirs, complete
+
+  # Our exitfunc uses this to know whether to delete the tokenized files
+  complete = False 
+
+  if jobs is None:
+    jobs = mp.cpu_count() + 4
+
+  b_dirs = [ tempfile.mkdtemp(prefix="tokenize-",suffix='-bucket', dir=outdir) for i in range(buckets) ]
+
+  # PASS 1: Tokenize documents into sets of terms
+   
+  # If there are few items, make the chunk size such that each job
+  # will have 2 chunks
+  chunk_size = max(1,min(len(items) / (jobs * 2), chunksize))
+  item_chunks = list(chunk(items, chunk_size))
+  tokenizer = Tokenizer(1,max_order)
+  pass_tokenize_globals = (tokenizer, b_dirs)
+  if jobs > 1:
+    with closing( mp.Pool(jobs, setup_pass_tokenize, pass_tokenize_globals)) as pool:
+      pass_tokenize_out = pool.imap_unordered(pass_tokenize, item_chunks, chunksize=1)
+  else:
+    setup_pass_tokenize(*pass_tokenize_globals)
+    pass_tokenize_out = imap(pass_tokenize, item_chunks)
+
+
+  doc_count = defaultdict(int)
+  chunk_count = len(item_chunks)
+  print "chunk size: {0} ({1} chunks)".format(chunk_size, chunk_count)
+  print "job count: {0}".format(jobs)
+
+  for i, keycount in enumerate(pass_tokenize_out):
+    print "tokenized chunk (%d/%d) [%d keys]" % (i+1,chunk_count, keycount)
+
+  if jobs > 1:
+    pool.join()
+
+  complete = True
+
+  return b_dirs
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-j","--jobs", type=int, metavar='N', help="spawn N processes (set to 1 for no paralleization)")
+  parser.add_argument("--buckets", type=int, metavar='N', help="distribute features into N buckets", default=NUM_BUCKETS)
+  parser.add_argument("-i", "--index", metavar='FILE', help="read index from FILE")
+  parser.add_argument("-o","--output", metavar='DIR', help="output buckets to DIR")
+  parser.add_argument("-b","--bucketlist", metavar='FILE', help="output paths to buckets to FILE")
+  parser.add_argument("--max_order", type=int, help="highest n-gram order to use", default=MAX_NGRAM_ORDER)
+  parser.add_argument("--chunksize", type=int, help="max chunk size (number of files to tokenize at a time - smaller should reduce memory use)", default=CHUNKSIZE)
+  
+  args = parser.parse_args()
+  
+  # check args
+  if not args.index:
+    parser.error("index(-i) must be specified")
+
+  if not args.output:
+    parser.error("output path(-o) must be specified")
+
+  makedir(args.output)
+
+  if args.bucketlist:
+    bucketlist_path = args.bucketlist
+  else:
+    bucketlist_path = os.path.basename(args.index) + '.bucketlist'
+
+  # display paths
+  print "index path:", args.index
+  print "bucketlist path:", bucketlist_path
+  print "output path:", args.output
+
+  with open(args.index) as f:
+    reader = csv.reader(f)
+    items = list(reader)
+
+  # Tokenize
+  print "will tokenize %d files" % len(items)
+  b_dirs = build_index(items, args.output, args.buckets, args.jobs, args.chunksize, max_order=args.max_order)
+
+  # output the paths to the buckets
+  with open(bucketlist_path,'w') as f:
+    for d in b_dirs:
+      f.write(d+'\n')
+
