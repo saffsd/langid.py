557f2ccdcdadccfb3b5d75c2ab09688200be36f4
Marco Lui <saffsd@gmail.com>
2013-01-30 15:48:31 +1100

    eliminated the need to load nb_features when training

--- langid/train/NBtrain.py
+++ langid/train/NBtrain.py
@@ -33,7 +33,8 @@ The views and conclusions contained in the software and documentation are those 
 authors and should not be interpreted as representing official policies, either expressed
 or implied, of the copyright holder.
 """
-FEATS_PER_CHUNK = 100
+MAX_CHUNK_SIZE = 100 # maximum number of files to tokenize at once
+NUM_BUCKETS = 64 # number of buckets to use in k-v pair generation
 
 import base64, bz2, cPickle
 import os, sys, argparse, csv
@@ -69,38 +70,41 @@ def state_trace(path):
       c[state] += 1
   return c
 
-def setup_pass1(nm_arr, output_states, state2feat, b_dirs, bucket_map):
+def setup_pass_tokenize(nm_arr, output_states, state2feat, b_dirs):
   """
   Set the global next-move array used by the aho-corasick scanner
   """
-  global __nm_arr, __output_states, __state2feat, __b_dirs, __bucket_map
+  global __nm_arr, __output_states, __state2feat, __b_dirs
   __nm_arr = nm_arr
   __output_states = output_states
   __state2feat = state2feat
   __b_dirs = b_dirs
-  __bucket_map = bucket_map
 
-def pass1(arg):
+def pass_tokenize(arg):
   """
   Tokenize documents and do counts for each feature
   Split this into buckets chunked over features rather than documents
   """
-  global __output_states, __state2feat, __b_dirs, __bucket_map
-  chunk_id, chunk_paths = arg
+  global __output_states, __state2feat, __b_dirs
+  chunk_offset, chunk_paths = arg
   term_freq = defaultdict(int)
   __procname = mp.current_process().name
   __buckets = [tempfile.mkstemp(prefix=__procname, suffix='.index', dir=p)[0] for p in __b_dirs]
 
-  for doc_id, path in enumerate(chunk_paths):
+  # Tokenize each document and add to a count of (doc_id, f_id) frequencies
+  for doc_count, path in enumerate(chunk_paths):
+    doc_id = doc_count + chunk_offset
     count = state_trace(path)
     for state in (set(count) & __output_states):
       for f_id in __state2feat[state]:
         term_freq[doc_id, f_id] += count[state]
 
+  # Distribute the aggregated counts into buckets
+  bucket_count = len(__buckets)
   for doc_id, f_id in term_freq:
-    bucket_index = __bucket_map[f_id]
+    bucket_index = hash(f_id) % bucket_count
     count = term_freq[doc_id, f_id]
-    item = ( f_id, chunk_id, doc_id, count )
+    item = ( f_id, doc_id, count )
     os.write(__buckets[bucket_index], marshal.dumps(item))
 
   for f in __buckets:
@@ -108,31 +112,30 @@ def pass1(arg):
 
   return len(term_freq)
 
-def setup_pass2(cm, chunk_offsets, num_instances):
-  global __cm, __chunk_offsets, __num_instances
+def setup_pass_ptc(cm, num_instances):
+  global __cm, __num_instances
   __cm = cm
-  __chunk_offsets = chunk_offsets
   __num_instances = num_instances
 
-def pass2(arg):
+def pass_ptc(b_dir):
   """
   Take a bucket, form a feature map, learn the nb_ptc for it.
   """
-  global __cm, __chunk_offsets, __num_instances
-  num_feats, base_f_id, b_dir = arg
-  fm = np.zeros((__num_instances, num_feats), dtype='int')
+  global __cm, __num_instances
+
+  terms = defaultdict(lambda : np.zeros((__num_instances,), dtype='int'))
 
   read_count = 0
   for path in os.listdir(b_dir):
     if path.endswith('.index'):
-      for f_id, chunk_id, doc_id, count in unmarshal_iter(os.path.join(b_dir, path)):
-        doc_index = __chunk_offsets[chunk_id] + doc_id
-        f_index = f_id - base_f_id
-        fm[doc_index, f_index] = count
+      for f_id, doc_id, count in unmarshal_iter(os.path.join(b_dir, path)):
+        terms[f_id][doc_id] = count
         read_count += 1
 
-  prod = np.dot(fm.T, __cm)
-  return read_count, prod
+  f_ids, f_vs = zip(*terms.items())
+  fm = np.vstack(f_vs)
+  prod = np.dot(fm, __cm)
+  return read_count, f_ids, prod
 
 
 def learn_pc(cm):
@@ -158,79 +161,49 @@ def generate_cm(items, num_classes):
 
   return cm
 
-def learn_ptc(paths, nb_features, tk_nextmove, state2feat, cm, job_count=None):
+def learn_ptc(paths, tk_nextmove, state2feat, cm, temp_path, args):
   global b_dirs
   num_instances = len(paths)
-  num_features = len(nb_features)
 
   # Generate the feature map
   nm_arr = mp.Array('i', tk_nextmove, lock=False)
 
-  # TODO: Magic const
-  if job_count:
-    chunk_size = min(len(paths) / (job_count*2), 100)
+  if args.jobs:
+    chunk_size = min(len(paths) / (args.jobs*2), args.chunk_size)
   else:
-    chunk_size = 100
-  path_chunks = list(chunk(paths, chunk_size))
-  feat_chunks = list(chunk(nb_features, FEATS_PER_CHUNK))
+    chunk_size = min(len(paths) / (mp.cpu_count()*2), args.chunk_size)
 
-  feat_index = index(nb_features)
-
-  bucket_map = {}
-  b_dirs = []
-  for chunk_id, feat_chunk in enumerate(feat_chunks):
-    for feat in feat_chunk:
-      bucket_map[feat_index[feat]] = chunk_id
-
-    b_dirs.append(tempfile.mkdtemp(prefix="train-",suffix="-bucket"))
-
+  # TODO: Set the output dir
+  b_dirs = [ tempfile.mkdtemp(prefix="train-",suffix='-bucket', dir=temp_path) for i in range(args.buckets) ]
 
   output_states = set(state2feat)
   
-  pass1_args = (nm_arr, output_states, state2feat, b_dirs, bucket_map)
-  with MapPool(job_count, setup_pass1, pass1_args) as f:
-    pass1_out = f(pass1, enumerate(path_chunks))
+  path_chunks = list(chunk(paths, chunk_size))
+  pass_tokenize_arg = zip(offsets(path_chunks), path_chunks)
+  
+  pass_tokenize_params = (nm_arr, output_states, state2feat, b_dirs) 
+  with MapPool(args.jobs, setup_pass_tokenize, pass_tokenize_params) as f:
+    pass_tokenize_out = f(pass_tokenize, pass_tokenize_arg)
 
-  write_count = sum(pass1_out)
+  write_count = sum(pass_tokenize_out)
   print "wrote a total of %d keys" % write_count
 
-  f_chunk_sizes = map(len, feat_chunks)
-  f_chunk_offsets = offsets(feat_chunks)
-  pass2_args = (cm, offsets(path_chunks), num_instances)
-  with MapPool(job_count, setup_pass2, pass2_args) as f:
-    pass2_out = f(pass2, zip(f_chunk_sizes, f_chunk_offsets, b_dirs))
+  pass_ptc_params = (cm, num_instances)
+  with MapPool(args.jobs, setup_pass_ptc, pass_ptc_params) as f:
+    pass_ptc_out = f(pass_ptc, b_dirs)
 
-  reads, pass2_out = zip(*pass2_out)
+  reads, ids, prods = zip(*pass_ptc_out)
   read_count = sum(reads)
-
   print "read a total of %d keys (%d short)" % (read_count, write_count - read_count)
-  prod = np.vstack(pass2_out)
+
+  prod = np.vstack(prods)[np.argsort(np.concatenate(ids))]
+  num_features = prod.shape[0]
   ptc = np.log(1 + prod) - np.log(num_features + prod.sum(0))
 
   nb_ptc = array.array('d')
   for term_dist in ptc.tolist():
     nb_ptc.extend(term_dist)
   return nb_ptc
-
-def read_corpus(path, wanted_langs = None):
-  print "data directory: ", path
-
-  if wanted_langs is not None:
-    wanted_langs = set(wanted_langs)
-
-  langs = set()
-  paths = []
-  for dirpath, dirnames, filenames in os.walk(path, followlinks=True):
-    for f in filenames:
-      lang = os.path.basename(dirpath)
-      if wanted_langs is None or lang in wanted_langs:
-        # None actually represents we want all langs
-        paths.append(os.path.join(dirpath, f))
-        langs.add(lang)
-
-  print "found %d files" % len(paths)
-  print "langs(%d): %s" % (len(langs), sorted(langs))
-  return paths, langs
 
 @atexit.register
 def cleanup():
@@ -250,12 +223,14 @@ if __name__ == "__main__":
   parser.add_argument("-o", "--output", metavar='OUTPUT', help="output langid.py-compatible model to OUTPUT")
   #parser.add_argument("-i","--index",metavar='INDEX',help="read list of training document paths from INDEX")
   parser.add_argument("model", metavar='MODEL_DIR', help="read index and produce output in MODEL_DIR")
+  parser.add_argument("--chunk_size", type=int, help='maximum chunk size (number of files)', default=MAX_CHUNK_SIZE)
+  parser.add_argument("--buckets", type=int, metavar='N', help="distribute features into N buckets", default=NUM_BUCKETS)
   args = parser.parse_args()
 
   if args.temp:
     temp_path = args.temp
   else:
-    raise NotImplementedError
+    temp_path = os.path.join(args.model, 'buckets')
 
   if args.scanner:
     scanner_path = args.scanner
@@ -269,7 +244,6 @@ if __name__ == "__main__":
 
   index_path = os.path.join(args.model, 'paths')
   lang_path = os.path.join(args.model, 'lang_index')
-  features_path = os.path.join(args.model, 'LDfeats')
 
   # display paths
   print "model path:", args.model
@@ -295,10 +269,9 @@ if __name__ == "__main__":
   cm = generate_cm(items, len(langs))
   paths = zip(*items)[1]
 
-  nb_features = read_features(features_path)
   nb_classes = langs
   nb_pc = learn_pc(cm)
-  nb_ptc = learn_ptc(paths, nb_features, tk_nextmove, state2feat, cm, args.jobs)
+  nb_ptc = learn_ptc(paths, tk_nextmove, state2feat, cm, temp_path, args)
 
   # output the model
   model = nb_ptc, nb_pc, nb_classes, tk_nextmove, tk_output
